aee45ef4ed39db5e84d8add1fa665e15b35d9e3f
sushagrawal <sushmitaagarwal1993@gmail.com>
2017-05-15 00:06:57 +0530

    scrap url without using asunc module

---
+++ app-without-async.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var _ = require('underscore');
+var https = require('https');
+app.listen(4000);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+console.log("Server is running");
+var maxRequest = 5;  // make 5 concurrent request at a time.
+var linkArray;
+var csvContent = "data:text/csv;charset=utf-8,";  
+app.get('/scrape', function(req, res){
+   
+    var url = "https://medium.com/" ;  //url to scrape
+    request(url, function(error, response, html){
+        if (error) {
+          throw error;
+        }
+        linkArray = makeLinkArray(html);
+        if( linkArray.length >0 ){
+            uploadToCSV(linkArray);
+            scrapeArray(linkArray);  //scrape further links.
+        }
+    
+    });
+    res.render('index');
+});
+
+
+function scrapeArray(linkArray){
+    
+    for(var i=0; i<maxRequest; i++){    // used for loop to make sure , max concurrent request would be 5. 
+        if(linkArray.length >0 ){
+            var url = linkArray[0];
+            linkArray.shift();
+            scrapNextUrl(url, linkArray);
+            
+        }
+    }
+}
+
+function scrapNextUrl(url, linkArray){
+    https.get(url, function(res) {
+        var pageData = "";
+        res.on('data', function (chunk) {
+            if(res.statusCode == 200){
+                pageData +=chunk;
+            }
+        });
+ 
+        res.on('end', function() {
+            var array = makeLinkArray(pageData);
+            uploadToCSV(array);
+            if(linkArray.length >0 ){
+                url = linkArray[0];
+                linkArray.shift();
+                scrapNextUrl(url, linkArray);  // make new connection after 5th after successful response.
+            }
+        });
+ 
+    }).on('error', function(e) {
+        if(linkArray.length >0 ){
+            url = linkArray[0];
+            linkArray.shift();
+            scrapNextUrl(url, linkArray); // make new connection after 5th after error response.
+        }
+    });
+}
+
+function makeLinkArray(body){
+    var that = this;
+    var linkArray = [];
+    $ = cheerio.load(body);
+    var hrefList = $("[href]");
+
+    if(hrefList.length > 0 ){
+        for(var i=0; i<hrefList.length ; i++){
+            var link  = hrefList[i].attribs['href'];
+            if (/^https?:\/\//i.test(link)) { // regex to check for http or https url in hrefs
+                if(!_.contains(linkArray,link)) { // if a link is already present in array then continue. 
+                    linkArray.push(link);
+                }
+            }
+        }
+    }
+    return linkArray;
+}
+
+function uploadToCSV(linkArray){
+    var csv  = linkArray.join(',');
+    fs.appendFile('linksnoasync.csv', csv, function(err) { // make csv file. 
+      if (err) throw err;
+    });
+}
+
--- app.js
+++ app.js
@@ -4,47 +4,75 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 var _ = require('underscore');
-var path = require('path');
-var http = require('http');
-http.globalAgent.maxSockets = 5;
+var async = require('async'); 
 app.listen(4000);
 
+//Description : Here I used async module , foreachlimit inbuilt method for limit=5 concurrent requests. 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-// app.set('view engine', 'ejs');
+
 console.log("Server is running");
 
 app.get('/scrape', function(req, res){
     var url = "https://medium.com/" ;
     request(url, function(error, response, html){
-        // console.log('res', res);
         if (error) {
           throw error;
         }
-        
-        var that = this;
-        var linkArray = [];
-        $ = cheerio.load(html);
-        
-
-        var hrefList = $("[href]");
-
-        if(hrefList.length > 0 ){
-          for(var i=0; i<hrefList.length ; i++){
-            var link  = hrefList[i].attribs['href'];
-            if(!_.contains(linkArray,link)) {
-              linkArray.push(link);
-            }
-          }
+        var linkArray = makeLinkArray(html);
+        if( linkArray.length >0 ){
+            uploadToCSV(linkArray);
+            scrapeEachLink(linkArray);
         }
-        // console.log('linkArray', linkArray.length);
-        var csv  = linkArray.join(',/n');
-        fs.appendFile('links.csv', csv, function(err) {
-          if (err) throw err;
-          console.log('file saved');
-        });
     
     });
     res.render('index');
 });
+
+function makeLinkArray(body){
+    var that = this;
+    var linkArray = [];
+    $ = cheerio.load(body);
+    var hrefList = $("[href]");
+
+    if(hrefList.length > 0 ){
+        for(var i=0; i<hrefList.length ; i++){
+            var link  = hrefList[i].attribs['href'];
+            if (/^https?:\/\//i.test(link)) {
+                if(!_.contains(linkArray,link)) {
+                    linkArray.push(link);
+                }
+            }
+        }
+    }
+    return linkArray;
+}
+
+function uploadToCSV(linkArray){
+    var csv  = linkArray.join(',');
+    fs.appendFile('links.csv', csv, function(err) {
+      if (err) throw err;
+    });
+}
+
+var requestApi = function(url, next){
+    if (/^https?:\/\//i.test(url)) {
+        request(url, function (error, response, html) {
+            var linkArray = makeLinkArray(html);
+            if( linkArray.length >0 ){
+                uploadToCSV(linkArray);
+                next(error); //this callback is for async to know about error or successful response. 
+            }
+        });
+    }
+};
+
+//async module used. 
+
+function scrapeEachLink(linkArray){
+    async.forEachLimit(linkArray, 5, requestApi, function(err){
+        if (err) throw err;
+        console.log('All requests processed!');
+    });
+}
+
